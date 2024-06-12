d3b0a6ee6c8d93642cb7ee513ae3d4ce670de381
mibazach <lobazamichal@student.agh.edu.pl>
2024-06-13 01:34:26 +0200

    lets go

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Load the game scene (make sure the scene is added in Build Settings)
+        SceneManager.LoadScene("MapScene"); // Replace "GameScene" with the name of your game scene
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Button Clicked");
+    }
+}
